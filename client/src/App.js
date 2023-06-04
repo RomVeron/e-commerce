0afb8b487acdde0a2a2e5d195441e2b705cfc359
Roman Veron <romanjuniorveron@hotmail.com>
2023-06-04 18:43:13 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,41 @@ import React from 'react';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Cart from './pages/Cart';
+export { createBrowserRouter, Outlet, RouterProvider, ScrollRestoration } from 'react-router-dom'
+
+const Layout = () =>{
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children:[
+      {
+        path:"/",
+        element:<Home />,
+      },
+      {
+        path:"/cart",
+        element:<Cart />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className='font-bodyFont'>
-      <Header />
-      <Home />
-      <Footer />
+      <RouterProvider router={router}/>
     </div>
   );
 }
