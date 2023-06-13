d8e0769deb989fc383c60f8ab368cc51d5c026e7
Roman Veron <romanjuniorveron@hotmail.com>
2023-06-13 19:37:45 -0400

    update

--- client/src/components/ProductsCard.js
+++ client/src/components/ProductsCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { BsArrowRight } from 'react-icons/bs'
 
 const ProductsCard = ({product}) => {
   
   return (
-    <div className='group'>
+    <div className='group relative'>
       <div className='w-full h-96 cursor-pointer overflow-hidden'>
         <img className='w-full h-full object-cover group-hover:scale-110 duration-500' src={product.image} alt='productImg'/>
       </div>
@@ -12,13 +13,29 @@ const ProductsCard = ({product}) => {
           <div>
             <h2 className='font-titleFont text-base font-bold'>{product.title.substring(0,15)}</h2>
           </div>
-          <div className='felx gap-2'>
-            <div className='text-sm relative w-28 flex gap-2 justify-end'>
+          <div className='felx gap-2 relative overflow-hidden w-28 text-sm'>
+            <div className='flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500'>
               <p className='line-through text-gray-500'>${product.oldPrice}</p>
               <p className='font-semibold'>${product.price}</p>
             </div>
+            <p className='absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500:'>
+              add to cart
+              <span>
+                <BsArrowRight />
+              </span>
+            </p>
           </div>
         </div>
+      <div>
+        <p>{product.category}</p>
+      </div>
+      <div className='absolute top-4 right-0'>
+        {
+          product.isNew && (
+            <p className='bg-black text-white font-semibold font-titleFont px-6 py-1'>Sale</p>
+          )
+        }
+      </div>
       </div>
     </div>
   )
